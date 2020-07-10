404235894d4625f110c5ae582c5ae51dd16e59dd
Branly Flores <cursosdecomputacion.net@gmail.com>
2020-07-09 18:25:52 -0600

    Agregando Ejercicio if

--- Ejercicios/Ejercicio09-IF/condiciones.cpp
+++ Ejercicios/Ejercicio09-IF/condiciones.cpp
@@ -7,15 +7,15 @@ int main(int argc, char const *argv[])
     int numeroSecreto = 7;
     int numero = 0;
 
-    cout << endl;
-
     cout << "Ingrese un numero:   ";
     cin >> numero;
- 
-    if (numero ==numeroSecreto){
+
+    cout << endl;
+
+    if (numero ==numeroSecreto) {
         cout << "ADIVINASTE EL NUMERO SECRETO: " << numero;
     } else{
-        cout << "ADIVINASTE:  " << numero;
+        cout << "NO ADIVINASTE:  " << numero;
     }
     
 
