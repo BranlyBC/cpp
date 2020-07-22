48ccc154c379bc4dfd45aa512d519fde73328e79
Branly Flores <cursosdecomputacion.net@gmail.com>
2020-07-21 18:54:14 -0600

    repaso 2

--- Ejercicios/Ejercicio27- Repaso 2/repaso.cpp
+++ Ejercicios/Ejercicio27- Repaso 2/repaso.cpp
@@ -1,24 +1,72 @@
 #include <iostream>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 using namespace std;
+
 int main(int argc, char** argv) {
 	
-	for (int i = 1; i <= 10; i++)
+	int opcion = 0;
+	
+	while (true)
 	{
-		cout << "indice:" << i;
+		system("cls");
+		
+		cout << "****";
+		cout << "MENU";
+		cout << "****";
+		
 		cout << endl;
+		cout << endl;
+		
+		cout << "1 - Bebidas Calientes" << endl;
+		cout << "2 - Bebidas Heladas" << endl;
+		cout << "3 - Reposteria" << endl;
+		cout << "0 - salir" << endl;
+		
+		cout << endl;
+		cout << "Ingrese una opcion entre 1 - 3:";
+		cin >> opcion;
+		
+		if (opcion == 0)
+		{
+			break;	
+		}
+		
+		if (opcion == 1)
+		{
+			system ("cls");
+			cout << "***************" << endl;
+			cout << "BEBIDAS CALIENTES" << endl;
+			cout << "***************" << endl;
+			system ("pause");
+		}
+		
+			if (opcion == 2)
+		{
+			system ("cls");
+			cout << "***************" << endl;
+			cout << "BEBIDAS HELADAS" << endl;
+			cout << "***************" << endl;
+			system ("pause");
+		}
+		
+			if (opcion == 3)
+		{
+			system ("cls");
+			cout << "**********" << endl;
+			cout << "REPOSTERIA" << endl;
+			cout << "**********" << endl;
+			system ("pause");
+		}
+		
+		if (opcion < 0 || opcion > 3)
+		{
+			system ("cls");
+			cout << "Ingrese una opcion valida" << endl;
+			system ("pause");
+		}	
 	}
 	
-	cout << endl;
-	cout << endl;
-	
-	int i = 0;
-	
-	while (i <= 10){
-		cout << "indice:" << i;
-		cout << endl;
-		i++;
-	}
 	return 0;
 }
