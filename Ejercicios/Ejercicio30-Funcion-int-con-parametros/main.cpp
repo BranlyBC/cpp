6e53912370b0152f5867f6ee438f7de9db2e68d6
Branly Flores <cursosdecomputacion.net@gmail.com>
2020-07-21 21:35:35 -0600

    agregando ejercicio con parametros

---
+++ Ejercicios/Ejercicio30-Funcion-int-con-parametros/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int sumar(int a, int b){
+	return a + b;
+}
+
+int restar(int a, int b){
+	return a - b;
+}
+
+int main(int argc, char** argv) {
+	
+	system("cls");
+	
+	int numero1 = 0;
+	int numero2 = 0;
+	
+	cout << "Ingrese el valor a:";
+	cout << "Ingrese el valor a:";
+	cin >> numero1;
+	cout << "Ingrese el valor de b:";
+	cin >> numero2;
+	
+	cout << endl;
+	
+	cout << "El resultado de la suma es:" << sumar(numero1, numero2);
+	cout << endl;
+	cout << "El resultado de la resta es:" << restar(numero1, numero2);
+	cout << endl;
+	
+	return 0;
+}
