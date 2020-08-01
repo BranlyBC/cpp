7cad58b5cdfbfcbecaa9f38ac4dea7ff50f8a075
Branly Flores <cursosdecomputacion.net@gmail.com>
2020-07-31 18:10:25 -0600

    Funciones con ciclos

---
+++ Ejercicios/Ejercicio35-Nombre-con-ciclo/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimircaracteres(char caracter, int ciclos)
+{
+	for(int i = 0; i < ciclos; i++)
+	{
+		cout << caracter;
+	}
+	
+	cout << endl;
+}
+
+int main(int argc, char** argv) {
+	
+	imprimircaracteres('@', 10);
+	imprimircaracteres('#', 5);
+	cout << "Hola Mundo" << endl;
+	imprimircaracteres('@', 1000);
+	
+	return 0;
+}
